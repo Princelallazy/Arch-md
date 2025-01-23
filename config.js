643dd1b38ce8bc1d4635ef887bd9372d6d790201
Princelallazy <ayobamio343@gmail.com>
2025-01-23 03:01:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"]
-global.botname = ["Arch Md"]
+global.owner = ["2347077816250"]
+global.botname = ["sharpblade"]
 global.simbol = "♘"
 global.typeMenu = 'v5' //don't change 
 global.anticall = false // set true to turn anticall always on
